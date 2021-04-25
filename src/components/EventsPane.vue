8e30a983f30adc59c86be672cd849c599bf24383
Marvin <mdtensuan_dev@protonmail.com>
2021-04-25 22:28:53 +0800

    Added EventsPane component.

---
+++ src/components/EventsPane.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container rounded" id="events-pane">
+        <h2>List of Events</h2>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'EventsPane',
+    components: {
+    }
+}
+</script>
+<style scoped>
+    #events-pane{
+        border-style: solid;
+        border-radius: 1px;
+        border-color: #770e6c;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
     <UserControlPanel v-bind:username='user' />
+    <EventsPane />
 </template>
 
 <script>
 import UserControlPanel from '../components/UserControlPanel'
+import EventsPane from '../components/EventsPane'
 
 export default {
   name: 'Home',
   components: {
-    UserControlPanel
+    UserControlPanel,
+    EventsPane
   },
   data (){
     return {

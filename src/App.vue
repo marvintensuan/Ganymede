021143998766aae8047ce6ca23a4c274f1ce3498
Marvin <mdtensuan_dev@protonmail.com>
2021-04-25 15:39:29 +0800

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="container">
-    <Header />
-    <router-view></router-view>
-    <Footer />
-  </div>
+  <Header />
+  <router-view></router-view>
+  <Footer />
 </template>
 
 <script>
@@ -26,6 +24,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

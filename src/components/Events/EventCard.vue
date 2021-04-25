4db43513c689c514c2635333bcc96145d534498c
Marvin <mdtensuan_dev@protonmail.com>
2021-04-25 22:53:25 +0800

    Added EventCard component with sample data.

---
+++ src/components/Events/EventCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card">
+        <div class="card-title">{{eventName}}</div>
+        <div class="card-body">
+            <p>Location: {{eventLocation}}</p>
+            <p>Date: {{eventDate}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EventCard',
+    props: {
+        eventName: String,
+        eventLocation: String,
+        eventDate: String
+    }
+}
+</script>
--- src/components/EventsPane.vue
+++ src/components/EventsPane.vue
@@ -1,14 +1,41 @@
 <template>
     <div class="container rounded" id="events-pane">
         <h2>List of Events</h2>
+        <div class="mb-3" v-for='event in sampleEvents' v-bind:key='event.id'>
+            <EventCard
+                v-bind:eventName='event.name'
+                v-bind:eventLocation='event.location'
+                v-bind:eventDate='event.date'
+            />
+        </div>
     </div>
 </template>
 
 <script>
+import EventCard from './Events/EventCard'
 
 export default {
     name: 'EventsPane',
     components: {
+        EventCard
+    },
+    data(){
+        return {
+            sampleEvents: [
+                {
+                    id: 1,
+                    name: 'Sample 1',
+                    location: 'Miami',
+                    date: '2021-01-25'
+                },
+                {
+                    id: 2,
+                    name: 'Sample 2',
+                    location: 'Detroit',
+                    date: '2021-02-14'
+                }
+            ]
+        }
     }
 }
 </script>
@@ -17,5 +44,8 @@ export default {
         border-style: solid;
         border-radius: 1px;
         border-color: #770e6c;
+    }
+    div{
+        text-align: justify;
     }
 </style>
